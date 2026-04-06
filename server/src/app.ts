74855daeac481e567213dbd9c26730111d2aa2fb
Luizzz1114 <cortesialuis1114@gmail.com>
2026-04-06 18:42:59 -0400

    fix: Change CORS config

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,9 +6,11 @@ import { generalLimiter } from './middlewares/limiter.middleware';
 
 const app: Application = express();
 
+const corsOrigin = process.env.CORS_ORIGIN || '*';
+
 app.use(helmet());
 app.use(generalLimiter);
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: corsOrigin }));
 app.use(express.json());
 
 app.use('/keeptab-api', router);
