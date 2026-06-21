514ef6859d0e01b0d0f2e26cda1e0689e84d5445
Luizzz1114 <cortesialuis1114@gmail.com>
2026-06-21 17:22:33 -0400

    fix: update CORS configuration to directly use CORS_ORIGIN environment variable

--- src/app.ts
+++ src/app.ts
@@ -9,13 +9,13 @@ const app: Application = express();
 
 app.set('trust proxy', 1);
 
-const allowedOrigins = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : ['http://localhost:5173'];
-
 app.use(helmet()); 
 
 app.use(cors({ 
-  origin: allowedOrigins,
-  credentials: true
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.use(generalLimiter);
